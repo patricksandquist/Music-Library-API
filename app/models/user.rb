86ed19635e0b650a455d415144a8f97efa7dbe0e
patricksandquist <patricksandquist@gmail.com>
2015-09-17 13:59:04 -0700

    Render user views

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@
 #
 
 class User < ActiveRecord::Base
-  attr_writer :password
+  attr_reader :password
 
   after_initialize :ensure_session_token
 
@@ -19,6 +19,8 @@ class User < ActiveRecord::Base
     :session_token,
     presence: true
   )
+
+  validates :password, length: { minimum: 5 }
 
   validates(
     :email,

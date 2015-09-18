7135a7a91187eaade2986d373f146b225104a6cd
patricksandquist <patricksandquist@gmail.com>
2015-09-17 18:15:21 -0700

    Add links and finish up for now

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,54 @@
 class AlbumsController < ApplicationController
+  def new
+    @album = Album.new
+    render :new
+  end
+
+  def create
+    @album = Album.new(album_params)
+    if @album.save
+      render :show
+    else
+      flash.now[:errors] = @album.errors.full_messages
+      render :new
+    end
+  end
+
+  def index
+    @albums = Album.all
+    render :index
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @album = Album.find(params[:id])
+    render :show
+  end
+
+  def update
+    @album = Album.find(params[:id])
+    if @album.update(album_params)
+      render :show
+    else
+      flash.now[:errors] = @album.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @album = Album.find(params[:id])
+    @album.destroy
+    @albums = Album.all
+    render :index
+  end
+
+  private
+
+  def album_params
+    params.require(:albums).permit(:name, :album_type, :band_id)
+  end
 end
